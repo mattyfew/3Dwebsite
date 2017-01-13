80c3eb92534062422136195e24f02ab7954014f5
Matt Fewer <mattfewer@hotmail.com>
2017-01-13 14:48:19 +0100

    testing

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+process.env.PWD = process.cwd()
 
 app.set('port', process.env.PORT || 8080);
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(process.env.PWD, 'public')));
 
 app.disable('x-powered-by');
 
