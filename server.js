26f0a232a026e8c50a26e8cc5f1b8a2525a58c22
Matt Fewer <mattfewer@hotmail.com>
2017-01-13 14:53:40 +0100

    trying again

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-process.env.PWD = process.cwd()
 
 app.set('port', process.env.PORT || 8080);
-app.use('/static', express.static(path.join(process.env.PWD, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.disable('x-powered-by');
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/public/index.html')
 })
+
 app.listen(app.get("port"), () => {
     console.log(`listening on port ${app.get("port")}`);
 });
