acd80fdb0ec1c80591e7d729cd20ec4f33d9bad8
Matt Fewer <mattfewer@hotmail.com>
2017-01-17 20:49:44 +0100

    changed to flycontrols, fixed plane tween, chopped controls

--- public/app.js
+++ public/app.js
@@ -1,14 +1,50 @@
 var camera, scene, renderer, container;
-var controls, trackballControls;
+var datControls, controls;
 var group, planeGroup, texture;
 var hemisphereLight, shadowLight;
 var textMesh, textMesh2, textPivot;
+var torusMesh, arrowMesh, buttonPivot, theTarget, runClickTweens;
 var planeRotation = 0;
 
 var mouse = { x: 0, y: 0 }, INTERSECTED;
 
 var loader = new THREE.TextureLoader();
 var clock = new THREE.Clock()
+
+function render(){
+    // scene.position.x = datControls.positionX
+    // scene.position.y = datControls.positionY
+    //
+    // scene.traverse(function(node){
+    //     if (node.name === "textPivot"){
+    //         node.position.x = datControls.textPivotPosX
+    //         node.position.y = datControls.textPivotPosY
+    //         node.position.z = datControls.textPivotPosZ
+    //     }
+    // })
+
+    buttonPivot.rotation.y += 0.1
+    TWEEN.update()
+
+    // if(runClickTweens === true){
+    //     runTweens()
+    // }
+
+    renderer.render(scene, camera);
+    var delta = clock.getDelta()
+    controls.update(delta)
+    requestAnimationFrame(render);
+}
+
+init()
+function init(){
+    createScene();
+    createText();
+    createPlaneGroup();
+    createTorus();
+    createLights();
+    render();
+}
 
 function createScene(){
 
@@ -32,10 +68,9 @@ function createScene(){
     nearPlane = 0.5;
     farPlane = 1000;
     camera = new THREE.PerspectiveCamera( fieldOfView, aspectRatio, nearPlane, farPlane );
+    camera.target = scene.position.clone();
+    camera.position.set(0,0,100)
 
-    camera.position.x = 0;
-    camera.position.z = 100;
-    camera.position.y = 0;
     container.appendChild(renderer.domElement)
 
     window.addEventListener('resize', handleWindowResize, false);
@@ -60,7 +95,7 @@ function createScene(){
     var skyBox = new THREE.CubeTextureLoader().setPath(imagePrefix).load(urls)
     scene.background = skyBox
 
-
+    /*
     // TRACKBALL CONTROLS
     // =============================================
 
@@ -72,12 +107,22 @@ function createScene(){
     trackballControls.noRotate = false;
     trackballControls.noZoom = false;
     trackballControlsMoving = true;
+    */
 
+    // FLYCONTROLS
+    // =============================================
+
+    controls = new THREE.FlyControls(camera)
+    controls.movementSpeed = 35;
+    controls.domElement = container
+    controls.rollSpeed = Math.PI / 24;
+    controls.autoForward = false;
+    controls.dragToLook = false;
 
     // DAT.GUI
     // =============================================
 
-    controls = new function(){
+    datControls = new function(){
         this.positionX = 0
         this.positionY = 0
 
@@ -86,47 +131,15 @@ function createScene(){
         this.textPivotPosZ = 0
     }
 
-    var gui = new dat.GUI();
-    gui.add(controls, 'positionX', -20, 20)
-    gui.add(controls, 'positionY', -20, 20)
-
-    var f1 = gui.addFolder('text')
-    f1.add(controls, 'textPivotPosX', -600, 600)
-    f1.add(controls, 'textPivotPosY', -600, 600)
-    f1.add(controls, 'textPivotPosZ', -600, 600)
-    f1.open()
-}
-
-// RENDER
-// =============================================
-
-function render(){
-    scene.position.x = controls.positionX
-    scene.position.y = controls.positionY
-
-    scene.traverse(function(node){
-        if (node.name === "textPivot"){
-            node.position.x = controls.textPivotPosX
-            node.position.y = controls.textPivotPosY
-            node.position.z = controls.textPivotPosZ
-        }
-    })
-
-    var delta = clock.getDelta()
-    trackballControls.update(delta)
-    TWEEN.update()
-
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
-}
-
-init()
-function init(){
-    createScene();
-    createText();
-    createLights();
-    createPlaneGroup();
-    render();
+    // var gui = new dat.GUI();
+    // gui.add(datControls, 'positionX', -20, 20)
+    // gui.add(datControls, 'positionY', -20, 20)
+    //
+    // var f1 = gui.addFolder('text')
+    // f1.add(datControls, 'textPivotPosX', -600, 600)
+    // f1.add(datControls, 'textPivotPosY', -600, 600)
+    // f1.add(datControls, 'textPivotPosZ', -600, 600)
+    // f1.open()
 }
 
 // =============================================
@@ -134,33 +147,31 @@ function init(){
 function createText(){
 
     var loader = new THREE.FontLoader();
+    textPivot = new THREE.Object3D();
+    textPivot.name = "textPivot"
+    textPivot.position.set(-65 ,5 ,0)
+
+    var textMaterial = new THREE.MeshPhongMaterial( {
+        specular: 0x53167d,
+        color: 0x53167d,
+        shininess: 100
+    } );
+
+    var options = {
+        // font: font,
+        size: 10,
+        height: 1,
+        curveSegments: 15,
+        bevelThickness: 1,
+        bevelSize: 1,
+        bevelEnabled: true,
+        bevelSegments: 15
+    }
 
     loader.load( '/fonts/optimer_regular.typeface.json', function ( font ) {
-        textPivot = new THREE.Object3D();
-        textPivot.name = "textPivot"
-        textPivot.position.x = -65
-        textPivot.position.y = 5
-        textPivot.position.z = 0
-
-        var options = {
-            font: font,
-            size: 10,
-            height: 1,
-            curveSegments: 15,
-            bevelThickness: 1,
-            bevelSize: 1,
-            bevelEnabled: true,
-            bevelSegments: 15
-        }
+        options.font = font;
 
         var textGeo = new THREE.TextGeometry( "Hello! I'm Matt Fewer.", options);
-
-        var textMaterial = new THREE.MeshPhongMaterial( {
-            specular: 0x53167d,
-            color: 0x53167d,
-            shininess: 100
-        } );
-
         textMesh = new THREE.Mesh( textGeo, textMaterial );
         textMesh.position.set( 0,0,0 );
         textMesh.name = "first_line"
@@ -171,8 +182,8 @@ function createText(){
 
         // SECOND LINE
         // =============================================
-
         textGeo = new THREE.TextGeometry( "I like to build websites.", options);
+
 
         textMesh2 = new THREE.Mesh( textGeo, textMaterial );
         textMesh2.position.set( 0,-30,0 );
@@ -182,71 +193,139 @@ function createText(){
     } );
 };
 
-function createPlane(x,y,name,img_path) {
+function createTorus(){
 
-    var planeGeo = new THREE.PlaneGeometry(20, 10);
+    buttonPivot = new THREE.Object3D();
+    buttonPivot.name = "buttonPivot"
+    buttonPivot.position.set(0, -40, 0)
 
-    var material = new THREE.MeshBasicMaterial({
-        map: new THREE.TextureLoader().load(img_path, function(texture){})
-    });
-    material.side = THREE.DoubleSide
+    // ARROW
 
+    var baseHeight = -4, baseWidth = 3;
 
+    var shape = new THREE.Shape();
 
-    var mesh = new THREE.Mesh(planeGeo, material)
-    mesh.name = name
-    mesh.position.set(x,y,0)
+    shape.moveTo( 0,0 );
+    shape.lineTo( baseWidth, 0 );
+    shape.lineTo( baseWidth, baseHeight );
+    shape.lineTo( baseWidth + 3, baseHeight );
+    shape.lineTo( baseWidth / 2, baseHeight * 2 );
+    shape.lineTo( -3, baseHeight );
+    shape.lineTo( 0, baseHeight );
+    shape.lineTo( 0, 0 );
 
-    scene.add(mesh)
-    planeGroup.add(mesh)
+    var extrudeSettings = {
+    	steps: 5,
+    	amount: 3,
+    	bevelEnabled: true,
+    	bevelThickness: 1,
+    	bevelSize: 1,
+    	bevelSegments: 1
+    };
+
+    var arrowGeom = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+    var arrowMaterial = new THREE.MeshNormalMaterial();
+    arrowMaterial.side = THREE.DoubleSide;
+    arrowMesh = new THREE.Mesh( arrowGeom, arrowMaterial );
+    arrowMesh.name = "arrowMesh"
+    arrowMesh.position.set(-1.5, 3.5, -2)
+
+    // TORUS
+
+    var torusMaterial = new THREE.MeshNormalMaterial();
+    torusMaterial.side = THREE.DoubleSide;
+
+    var torusGeom = new THREE.TorusGeometry(10, 2, 4, 6, Math.PI * 2);
+    torusMesh = new THREE.Mesh(torusGeom, torusMaterial);
+    torusMesh.name = "torusMesh"
+
+    buttonPivot.add(arrowMesh)
+    buttonPivot.add(torusMesh)
+    scene.add(buttonPivot)
 }
 
 function createPlaneGroup(){
     planeGroup = new THREE.Group();
 
-    createPlane(-25, -20, "plane1", "/img/websites/advanced_cosmetic_dentistry.jpg")
-    createPlane(0, -20, "plane2", "/img/websites/rexmenu.jpg")
-    createPlane(25, -20, "plane3", "/img/websites/dr_maieve.jpg")
+    var startPosY = -60
 
-    createPlane(-25, -40, "plane3", "/img/websites/dr_alison_black.jpg")
-    createPlane(0, -40, "plane5", "/img/websites/atlas_vein.jpg")
-    createPlane(25, -40, "plane6", "/img/websites/galleria.jpg")
+    createPlane(-25, startPosY - 20, "plane1", "/img/websites/advanced_cosmetic_dentistry.jpg")
+    createPlane(0, startPosY - 20, "plane2", "/img/websites/rexmenu.jpg")
+    createPlane(25, startPosY - 20, "plane3", "/img/websites/dr_maieve.jpg")
 
-    createPlane(-25, -60, "plane7", "/img/websites/priest_dental.jpg")
-    createPlane(0, -60, "plane8", "/img/websites/retinal_san_antonio.jpg")
-    createPlane(25, -60, "plane9", "/img/websites/schlessinger.jpg")
+    createPlane(-25, startPosY - 40, "plane3", "/img/websites/dr_alison_black.jpg")
+    createPlane(0, startPosY - 40, "plane5", "/img/websites/atlas_vein.jpg")
+    createPlane(25, startPosY - 40, "plane6", "/img/websites/galleria.jpg")
 
-    createPlane(-25, -80, "plane10", "/img/websites/static_home.jpg")
-    createPlane(0, -80, "plane11", "/img/websites/tsrh_home.jpg")
-    createPlane(25, -80, "plane12", "/img/websites/urogynecology_center.jpg")
+    createPlane(-25, startPosY - 60, "plane7", "/img/websites/priest_dental.jpg")
+    createPlane(0, startPosY - 60, "plane8", "/img/websites/retinal_san_antonio.jpg")
+    createPlane(25, startPosY - 60, "plane9", "/img/websites/schlessinger.jpg")
 
-    createPlane(-25, -100, "plane13", "/img/websites/gentle_dental.jpg")
-    createPlane(0, -100, "plane14", "/img/websites/mcdowell.jpg")
-    createPlane(25, -100, "plane15", "/img/websites/dental_phobia.jpg")
+    createPlane(-25, startPosY - 80, "plane10", "/img/websites/static_home.jpg")
+    createPlane(0, startPosY - 80, "plane11", "/img/websites/tsrh_home.jpg")
+    createPlane(25, startPosY - 80, "plane12", "/img/websites/urogynecology_center.jpg")
 
-    createPlane(-25, -120, "plane16", "/img/websites/belcor_builders.jpg")
-    createPlane(0, -120, "plane17", "/img/websites/eye_surgery_center.jpg")
-    createPlane(25, -120, "plane18", "/img/websites/centerderm.jpg")
+    createPlane(-25, startPosY - 100, "plane13", "/img/websites/gentle_dental.jpg")
+    createPlane(0, startPosY - 100, "plane14", "/img/websites/mcdowell.jpg")
+    createPlane(25, startPosY - 100, "plane15", "/img/websites/dental_phobia.jpg")
 
-    createPlane(-25, -140, "plane19", "/img/websites/smiles_4_a_lifetime.jpg")
-    createPlane(0, -140, "plane20", "/img/websites/tennessee_vein.jpg")
-    createPlane(25, -140, "plane21", "/img/websites/the_vein_clinic.jpg")
+    createPlane(-25, startPosY - 120, "plane16", "/img/websites/belcor_builders.jpg")
+    createPlane(0, startPosY - 120, "plane17", "/img/websites/eye_surgery_center.jpg")
+    createPlane(25, startPosY - 120, "plane18", "/img/websites/centerderm.jpg")
+
+    createPlane(-25, startPosY - 140, "plane19", "/img/websites/smiles_4_a_lifetime.jpg")
+    createPlane(0, startPosY - 140, "plane20", "/img/websites/tennessee_vein.jpg")
+    createPlane(25, startPosY - 140, "plane21", "/img/websites/the_vein_clinic.jpg")
 
     scene.add(planeGroup);
+
+    function createPlane(x,y,name,img_path) {
+        var planeGeo = new THREE.PlaneGeometry(20, 10);
+        var material = new THREE.MeshBasicMaterial({
+            map: new THREE.TextureLoader().load(img_path, function(texture){})
+        });
+        material.side = THREE.DoubleSide
+
+        var mesh = new THREE.Mesh(planeGeo, material)
+        mesh.name = name
+        mesh.position.set(x,y,0)
+
+        scene.add(mesh)
+        planeGroup.add(mesh)
+    }
 }
 function createLights() {
+    /*
     // AMBIENT LIGHT
 
     var ambiColor = "#ffffff"
     var ambientLight = new THREE.AmbientLight(ambiColor, 1)
-    // scene.add(ambientLight)
+    scene.add(ambientLight)
+    */
 
+    /*
     // POINT LIGHT
 
     var pointColor = "#ffffff";
     var pointLight = new THREE.PointLight(pointColor);
     pointLight.position.set(10,10,10);
     scene.add(pointLight);
+    */
+
+    // SPOTLIGHT
+
+    var pointColor = "#ffffff";
+    var spotLight = new THREE.SpotLight(pointColor);
+    spotLight.position.set(0,0,90);
+    spotLight.castShadow = true;
+
+    var target = new THREE.Object3D();
+    target.position = new THREE.Vector3(5, 0, 0);
+    spotLight.target = target;
+    scene.add(spotLight);
+
+
+
 
 }
 
@@ -257,15 +336,13 @@ function onDocumentMouseDown(event) {
     var intersects = raycaster.intersectObjects(scene.children, true);
 
 
-    // PORTFOLIO PLANE FLIP TWEEN
+    // PLANE FLIP TWEEN
     // =============================================
 
     var targetPosition, currentPosition;
-    if (intersects.length > 0 && intersects[0].object.name != "second_line" && intersects[0].object.name != "first_line") {
-
+    if (intersects.length > 0 && intersects[0].object.name.substring(0,5) === "plane") {
 
         // TWEENBACK
-        // =============================================
 
         if (intersects[0].object.rotation.y > 0){
 
@@ -275,7 +352,6 @@ function onDocumentMouseDown(event) {
                 z: intersects[0].object.position.z,
                 rot: 360
             }
-            console.log(currentPosition);
 
             var tweenBack = new TWEEN.Tween(currentPosition).to(firstPosition, 1000)
                 .easing(TWEEN.Easing.Exponential.Out).onUpdate( function () {
@@ -286,9 +362,7 @@ function onDocumentMouseDown(event) {
                 })
             tweenBack.start()
 
-
         // TWEEN
-        // =============================================
 
         } else {
             firstPosition = {
@@ -297,7 +371,7 @@ function onDocumentMouseDown(event) {
                 z: intersects[0].object.position.z,
                 rot: 0
             }
-            currentPosition = {
+            camCurrentPosition = {
                 x: intersects[0].object.position.x,
                 y: intersects[0].object.position.y,
                 z: intersects[0].object.position.z,
@@ -310,12 +384,26 @@ function onDocumentMouseDown(event) {
                 rot: camera.rotation._y + 360
             }
 
-            var tween = new TWEEN.Tween(currentPosition).to(targetPosition, 2000)
-                .easing(TWEEN.Easing.Exponential.Out).onUpdate( function () {
-                    intersects[0].object.rotation.y = (currentPosition.rot * Math.PI)/180
-                    intersects[0].object.position.x = currentPosition.x
-                    intersects[0].object.position.y = currentPosition.y
-                    intersects[0].object.position.z = currentPosition.z
+            var tween = new TWEEN.Tween(camCurrentPosition).to(targetPosition, 2000)
+                .easing(TWEEN.Easing.Exponential.Out)
+                .onStart(function() {
+                    console.log(targetPosition);
+                    console.log(camCurrentPosition);
+                })
+                .onUpdate( function () {
+                    // intersects[0].object.rotation.y = (currentPosition.rot * Math.PI)/180
+                    // intersects[0].object.position.x = currentPosition.x
+                    // intersects[0].object.position.y = currentPosition.y
+                    // intersects[0].object.position.z = currentPosition.z
+
+                    intersects[0].object.rotation.y = (camCurrentPosition.rot * Math.PI)/180
+                    intersects[0].object.position.x = camCurrentPosition.x
+                    intersects[0].object.position.y = camCurrentPosition.y
+                    intersects[0].object.position.z = camCurrentPosition.z
+
+                    // console.log(intersects[0].object.position);
+                    // console.log(camCurrentPosition);
+
                 })
             tween.start()
         }
@@ -324,12 +412,10 @@ function onDocumentMouseDown(event) {
     // NAVIGATION
     // =============================================
 
-    } else if (intersects[0].object.name === "navMesh") {
-        console.log(intersects[0].object.name + " selected");
+    } else if (intersects.length > 0 && ( intersects[0].object.name === "torusMesh" || intersects[0].object.name === "arrowMesh" )) {
 
-        // intersects[0].object.position.x = 180;
+        runClickTweens = true
 
-        // camera.position.x = -100
     }
 }
 function handleWindowResize() {
@@ -339,4 +425,52 @@ function handleWindowResize() {
 	renderer.setSize(WIDTH, HEIGHT);
 	camera.aspect = WIDTH / HEIGHT;
 	camera.updateProjectionMatrix();
+}
+
+function runTweens(){
+    var theTarget = new THREE.Object3D();
+    theTarget.name = "theTarget"
+    theTarget.position.set(0,0,0)
+    camera.target = theTarget
+    camera.updateProjectionMatrix();
+
+    var tween1 = new TWEEN.Tween(camera.position).to({
+        y: -140}, 5000)
+        .easing(TWEEN.Easing.Exponential.Out)
+        .onUpdate(function () {
+            trackballControls.target.set(theTarget.position);
+            // camera.lookAt(theTarget);
+            // camera.target.position.copy( theTarget );
+            // camera.updateProjectionMatrix();
+
+        })
+        // .onComplete(function () {
+        //     // camera.lookAt(theTarget);
+        //     // camera.target.position.copy( theTarget );
+        //     // camera.updateProjectionMatrix();
+        // })
+
+    var tween2 = new TWEEN.Tween(trackballControls.target).to({
+        y: -140}, 5000)
+        .easing(TWEEN.Easing.Exponential.Out)
+        .onUpdate(function () {
+            // console.log(trackballControls.target)
+            trackballControls.target.set(theTarget.position);
+
+            // camera.lookAt(theTarget);
+            // camera.target.position.copy( theTarget );
+            // camera.updateProjectionMatrix();
+        })
+        .onComplete(function () {
+            trackballControls.reset()
+
+        //     // camera.lookAt(theTarget);
+        //     // camera.target.position.copy( theTarget );
+        //     // camera.updateProjectionMatrix();
+        //     console.log("complete");
+        //     runClickTweens = false
+        })
+
+        tween2.start()
+    tween1.start()
 }
