71841b3e815b2f87b8ed0e4560427eb9c88b074d
Matt Fewer <mattfewer@hotmail.com>
2017-01-29 23:38:34 +0100

    split routes, 3d for desktop, 2d for mobile, refactored files

--- server.js
+++ server.js
@@ -3,25 +3,33 @@ const path = require('path');
 const app = express();
 const http = require('http');
 const https = require('https');
-const fs = require('fs')
+const fs = require('fs');
+const device = require('express-device');
 
 // fs.exists('./public', console.log);
-// console.log(__dirname);
-
-app.set('port', process.env.PORT || 8080);
-app.use(express.static('public'));
-app.use(require("cors")())
-
+// app.use(require("cors")())
 // app.disable('x-powered-by');
 
-app.get('/', (req,res)=>{
-    console.log("somthing");
-    res.sendFile(__dirname + '/public/index.html')
+app.set('port', process.env.PORT || 8080);
+app.use('/3dwebsite', express.static(path.join(__dirname, 'public/3d')));
+app.use('/2dwebsite', express.static(path.join(__dirname, 'public/2d')));
+app.use(device.capture())
+
+app.get('/', function(req,res){
+    console.log("client device: " + req.device.type);
+    req.device.type === "desktop" ?
+        res.sendFile(__dirname + '/public/3d/html/index.html') :
+        res.sendFile(__dirname + '/public/2d/index.html') ;
 })
 
 app.listen(app.get("port"), () => {
     console.log(`listening on port ${app.get("port")}`);
 });
+
+
+
+
+
 
 // var options = {
 //   key: fs.readFileSync('key.pem').toString(),
