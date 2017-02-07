796092f05f607964a5f728665da76a9b45344841
Matt Fewer <mattfewer@hotmail.com>
2017-02-08 00:05:42 +0100

    this works on local

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ const app = express();
 const path = require('path');
 const device = require('express-device');
 const hbs = require('express-handlebars');
-const fs = require('fs');
-// const favicon = require('serve-favicon');
 
 app.set('port', process.env.PORT || 8080);
 app.use('/3dwebsite', express.static(path.join(__dirname, 'public/3d')));
 app.use('/2dwebsite', express.static(path.join(__dirname, 'public/2d')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(device.capture())
-
-// app.use(favicon(path.join(__dirname,'public','img','favicon.png')));
 
 app.engine('handlebars', hbs({
     layoutsDir: './public/2d/views/layouts',
@@ -26,7 +22,9 @@ app.get('/', function(req,res){
     console.log("client device: " + req.device.type);
     req.device.type === "desktop" ?
         res.sendFile(__dirname + '/public/3d/html/index.html') :
-        res.render('./index.handlebars', { data : require('./public/data.json') })
+        res.render('./index.handlebars', { 
+            data : require('./public/data.json')
+        })
 
 })
 
